9ade903e9092524c4281a868bfdafa1a79d4c3ae
lenakernytska <yaapril222@gmail.com>
2021-03-26 22:17:05 +0200

    Start

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import styles from "./Navigation.module.css";
+
+const Navigation = () => (
+    <nav className={styles.nav}>
+        <NavLink exact to="/" className={styles.link} activeClassName={styles.activeLink}>Home</NavLink>
+        <NavLink to="/movies" className={styles.link} activeClassName={styles.activeLink}>Movies</NavLink>
+    </nav>
+);
+
+export default Navigation;
